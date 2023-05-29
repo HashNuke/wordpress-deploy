5dbfec15f6b92c06ddf8a725f3ca18ed155cc82e
Akash Manohar <akash@akash.im>
2023-05-29 08:27:50 +0700

    Update formatting

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 python3 /app/scripts/write-hosts-ini.py
-echo "\n==> Inventory file:"
+echo ""
+echo "==> Inventory file:"
 cat hosts.ini
-echo "\n==> Running Ansible playbook..."
+echo ""
+echo "==> Running Ansible playbook..."
 
 ansible-playbook -i hosts.ini --extra-vars @/site.yml $@
